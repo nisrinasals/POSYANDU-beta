fd9d4ba911f9b4ad66eccd82d14583032d3efdc1
nisrina salsa <nisrinahikmawan@gmail.com>
2026-09-10 22:51:12 +0700

    feat: add mailer utility for sending emails using nodemailer

---
+++ utils/mailer.js
@@ -0,0 +1,22 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT) || 587,
+  secure: process.env.SMTP_SECURE === "true",
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+const sendEmail = async ({ to, subject, html }) => {
+  return transporter.sendMail({
+    from: process.env.SMTP_FROM || process.env.SMTP_USER,
+    to,
+    subject,
+    html,
+  });
+};
+
+module.exports = sendEmail;
