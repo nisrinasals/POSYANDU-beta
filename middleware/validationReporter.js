f7b73e47a87198c910c1bfd520cccdb19dbaef5e
nisrina salsa <nisrinahikmawan@gmail.com>
2026-09-14 13:34:24 +0700

    feat: refactor validation logic to use centralized validator object in API routes

--- middleware/validationReporter.js
+++ middleware/validationReporter.js
@@ -1,8 +1,31 @@
 "use strict";
 const { validationResult } = require("express-validator");
 
+const authValidators = require("./validators/authValidators");
+const commonValidators = require("./validators/common");
+const kehamilanValidator = require("./validators/kehamilanValidator");
+const kunjunganValidators = require("./validators/kunjunganValidators");
+const pemeriksaanValidators = require("./validators/pemeriksaanValidators");
+const posyanduValidators = require("./validators/posyanduValidators");
+const sesiPosyanduValidators = require("./validators/sesiPosyanduValidators");
+const userValidators = require("./validators/userValidators");
+const wargaValidators = require("./validators/wargaValidators");
+
+const validator = {
+  auth: authValidators,
+  common: commonValidators,
+  kehamilan: kehamilanValidator,
+  kunjungan: kunjunganValidators,
+  pemeriksaan: pemeriksaanValidators,
+  posyandu: posyanduValidators,
+  sesiPosyandu: sesiPosyanduValidators,
+  user: userValidators,
+  warga: wargaValidators,
+};
+
 const validateResult = (req, res, next) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     return res.status(400).json({
       status: "error",
@@ -13,7 +36,11 @@ const validateResult = (req, res, next) => {
       })),
     });
   }
+
   next();
 };
 
-module.exports = validateResult;
+module.exports = {
+  validator,
+  validateResult,
+};
