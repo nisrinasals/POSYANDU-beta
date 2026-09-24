b7cb86c10fdc1ae2e7a8c7f78814b1d62d7e36fd
nisrina salsa <nisrinahikmawan@gmail.com>
2026-09-25 00:17:31 +0700

    Remove duplicate files

--- frontend/src/src/components/pemeriksaan/DetailRekapPemeriksaanModal.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DetailRekapModal, { resolve5StepDetails } from './DetailRekapModal';
-
-export { resolve5StepDetails };
-
-export default function DetailRekapPemeriksaanModal(props) {
-  return <DetailRekapModal {...props} />;
-}
